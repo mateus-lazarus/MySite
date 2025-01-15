9e72b3b79df1929c2f258fb8f59fd5e5a8fb1649
mateus.lazarus <devmateuslazarus@gmail.com>
2025-01-14 23:10:05 -0300

    update

--- dockerDeployToServer.sh
+++ dockerDeployToServer.sh
@@ -1,6 +1,9 @@
 sudo docker-compose build
 sudo docker save -o ~/my-site-web_node-web-app.tar my-site-web
 sudo scp ~/my-site-web_node-web-app.tar mateusadminserver@192.168.0.154:~/my-site-web_node-web-app.tar
-ssh my-server -t "sudo docker load -i ~/my-site-web_node-web-app.tar & sudo docker run -d --name my-site-web-container --network nginx_proxy_manager_default -p 45455:5000 my-site-web:latest" 
+
+# The last line is not working as expected because of the &
+# It is important to remove the image from my docker local and the .tar from the server
+sudo ssh my-server -t "sudo docker load -i ~/my-site-web_node-web-app.tar & sudo docker run -d --name my-site-web-container --network nginx_proxy_manager_default -p 45455:5000 my-site-web:latest" 
 
 
